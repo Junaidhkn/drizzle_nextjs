e35ea1cff8c68303b4b62b83c4a895dbb413559f
junaidhkn <junaidhkniazi@gmail.com>
2024-08-03 21:11:33 +0500

    drizzle setup

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,8 +1,14 @@
-import { config } from "dotenv";
-import { expand } from "dotenv-expand";
-import { z, ZodError } from "zod";
+import { config } from 'dotenv';
+import { expand } from 'dotenv-expand';
+import { z, ZodError } from 'zod';
 
 const envSchema = z.object({
+	DB_HOST: z.string().min(1),
+	DB_USER: z.string().min(1),
+	DB_PASSWORD: z.string().min(1),
+	DB_NAME: z.string().min(1),
+	DB_PORT: z.coerce.number().min(1),
+	DATABASE_URL: z.string().min(1),
 	AUTH_SECRET: z.string().min(1),
 	AUTH_TRUST_HOST: z.string().min(1),
 });
@@ -13,7 +19,7 @@ try {
 	envSchema.parse(process.env);
 } catch (e) {
 	if (e instanceof ZodError) {
-		console.error("Environment validation error:", e.errors);
+		console.error('Environment validation error:', e.errors);
 	}
 }
 
