fab2eebb3ffdd04888ba8f08044aee5020b13d63
junaidhkn <junaidhkniazi@gmail.com>
2024-08-03 23:07:43 +0500

    comments schema

--- src/db/schema/comments.ts
+++ src/db/schema/comments.ts
@@ -1,7 +1,23 @@
 import {
+	AnyPgColumn,
 	integer,
 	pgTable,
 	serial,
+	text,
 	timestamp,
 	varchar,
 } from 'drizzle-orm/pg-core';
+
+import { user } from '@/db/schema';
+
+export const comment = pgTable('comment', {
+	id: serial('id').primaryKey(),
+	parentId: integer('parent_id').references((): AnyPgColumn => comment.id),
+	userId: integer('user_id')
+		.references(() => user.id)
+		.notNull(),
+	content: text('content').notNull(),
+	postId: integer('post_id').notNull(),
+	createdAt: timestamp('created_at', { mode: 'string' }).notNull().defaultNow(),
+	updatedAt: timestamp('updated_at', { mode: 'string' }).notNull().defaultNow(),
+});
