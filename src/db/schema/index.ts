665bd0645a7a8572af3d9a8d59f183f89b61785c
junaidhkn <junaidhkniazi@gmail.com>
2024-08-06 01:32:04 +0500

    drizzle migration

--- src/db/schema/index.ts
+++ src/db/schema/index.ts
@@ -1,2 +1,6 @@
 export { user } from '@/db/schema/user';
 export { category } from '@/db/schema/category';
+export { comment, commentRelations } from '@/db/schema/comments';
+export { post, postRelations } from '@/db/schema/post';
+export { postTags, postTagRelations } from '@/db/schema/postTags';
+export { tag, tagRelations } from '@/db/schema/tag';
